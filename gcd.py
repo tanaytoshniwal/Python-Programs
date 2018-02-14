0d8a49738aaadb5f9b9294744787c843947a652e
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-02-15 02:12:35 +0530

    updated print function

--- gcd.py
+++ gcd.py
@@ -15,7 +15,13 @@ def gcd(a, b):
             common_factors.append(i)
     return (common_factors[-1])
 
+def p(s):
+    print(s)
+
 if __name__ == "__main__":
+    p("input a:")
     a = int(input())
+    p("input b:")
     b = int(input())
+    p("gcd of "+str(a)+" and "+str(b)+" is:")
     print(gcd(a, b))
