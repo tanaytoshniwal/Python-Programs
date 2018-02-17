6255c328b46b6977ae2771d2e0967acb7fb29fc2
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-02-18 03:16:06 +0530

    sum of squares

--- sumofsquares/sumofsquares.py
+++ sumofsquares/sumofsquares.py
@@ -1,7 +1,10 @@
 def sumofsquares(n):
-	for i in range(1, sqrt(n)):
-		for j in range(i, sqrt(n)):
-			if(i*i+j*j == n):
-				return(True)
-			else:
-				return(False)
+    i = 1
+    while i * i <= n :
+        j = 1
+        while(j * j <= n) :
+            if (i * i + j * j == n) :
+                return True
+            j = j + 1
+        i = i + 1
+    return False
